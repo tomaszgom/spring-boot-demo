7459eefbd758b693a39bde421acd148049f4ca60
Tomo <t.gomoradzki@gmail.com>
2019-07-12 01:44:06 +0200

    Modify app.properties, adding methods in TaskController

--- spring-boot-demo-api/src/main/java/com/tomgom/springbootdemo/restcontroller/ProjectTaskRestController.java
+++ spring-boot-demo-api/src/main/java/com/tomgom/springbootdemo/restcontroller/ProjectTaskRestController.java
@@ -3,9 +3,11 @@ package com.tomgom.springbootdemo.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,13 +44,34 @@ public class ProjectTaskRestController {
 	}
 	
 	//Add new Project Task
-	@PostMapping("/project-tasks/")
+	@PostMapping("/project-tasks")
 	public ProjectTask addProjectTask(@RequestBody ProjectTask theProjectTask) {
 		
 		theProjectTask.setId(0);	
 		projectTaskService.saveTask(theProjectTask);
 		return theProjectTask;
 	}
+	
+	// Update Project Task
+	@PutMapping("/project-tasks")
+	public ProjectTask updateProjectTask(@RequestBody ProjectTask theProjectTask) {
+			
+		projectTaskService.saveTask(theProjectTask);
+		return theProjectTask;
+	}
+	
+	@DeleteMapping("/project-tasks/{projectTaskId}")
+	public String deleteProjectTask(@PathVariable int projectTaskId ){
+		ProjectTask theProjectTask = projectTaskService.searchById(projectTaskId);
+
+		if(theProjectTask == null) {
+			throw new RuntimeException("Employee id not found - " + projectTaskId);
+		}
+		
+		projectTaskService.deleteTaskById(projectTaskId);
+		return "Project Task Id '" + theProjectTask + "' has been deleted.";
+	}
+	
 	
 	
 	@GetMapping ("/test")
